0d8ff77e04c1abc7e4fe3cc90d524413eac7b899
FaridGanbarov <faridqanbarov3@gmail.com>
2026-04-24 11:41:44 +0400

    Add SMTP email sending via Gmail in consumer service

--- consumer-service/src/main/java/com/example/consumerservice/listener/EmailConsumer.java
+++ consumer-service/src/main/java/com/example/consumerservice/listener/EmailConsumer.java
@@ -1,13 +1,31 @@
 package com.example.consumerservice.listener;
 
 import com.example.common.EmailMessage;
+import com.example.consumerservice.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
-@Service
+@Slf4j
+@Component
+@RequiredArgsConstructor
 public class EmailConsumer {
+
+    private final EmailService emailService;
+
     @RabbitListener(queues = "email.queue")
-    public void receiveMessage(EmailMessage emailMessage) {
-        System.out.println("Received: " + emailMessage);
+    public void receiveMessage(EmailMessage message) {
+
+        log.info("Received message: {}", message);
+
+        try {
+            emailService.sendEmail(message);
+            log.info("Email sent successfully!");
+
+        } catch (Exception e) {
+            log.error("Failed to send email: {}", e.getMessage());
+        }
     }
 }
